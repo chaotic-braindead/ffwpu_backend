a6c48e73ed6c95861e33de0495f30873855b27a4
Rafael Torres <rafaelsebastian0908@gmail.com>
2025-03-01 15:43:46 +0800

    add donations

--- ffwpu_backend/apps/donations/urls.py
+++ ffwpu_backend/apps/donations/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.DonationListCreate.as_view()),
+    path("/<int:donation_id>", views.DonationRetrieveDeleteUpdate.as_view()),
 ]
--- ffwpu_backend/apps/donations/views.py
+++ ffwpu_backend/apps/donations/views.py
@@ -1,5 +1,19 @@
-from django.http import HttpResponse
+from rest_framework import generics
+from rest_framework.filters import OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Donation
+from .serializers import DonationSerializer
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the donations index.")
+class DonationListCreate(generics.ListCreateAPIView):
+    queryset = Donation.objects.select_related("member")
+    serializer_class = DonationSerializer
+    model = serializer_class.Meta.model
+    filter_backends = [OrderingFilter, DjangoFilterBackend]
+    permission_classes = []
+
+
+class DonationRetrieveDeleteUpdate(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Donation.objects.select_related("member")
+    serializer_class = DonationSerializer
+    model = serializer_class.Meta.model
