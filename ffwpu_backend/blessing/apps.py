3dfbaf05aef479c5b81e24e86e22be915f9943b5
Rafael Torres <rafaelsebastian0908@gmail.com>
2025-04-22 16:33:51 +0800

    init

---
+++ ffwpu_backend/blessing/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BlessingConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'blessing'
---
+++ ffwpu_backend/ffwpu_backend/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for ffwpu_backend project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ffwpu_backend.settings')
+
+application = get_asgi_application()
