104d92fd3c584c027afe1615981f84c9af3ca8c4
Rafael Torres <rafaelsebastian0908@gmail.com>
2025-05-09 20:29:44 +0800

    add checks for duplicate church, region, subregion

--- ffwpu_backend/church/views.py
+++ ffwpu_backend/church/views.py
@@ -1,5 +1,7 @@
 from rest_framework import viewsets
 from .serializers import *
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 
@@ -7,3 +9,19 @@ from .serializers import *
 class ChurchViewSet(viewsets.ModelViewSet):
     queryset = Church.objects.all()
     serializer_class = ChurchSerializer
+
+    def create(self, request, *args, **kwargs):
+        # Check if the church name already exists
+        church_name = request.data.get("name")
+        region_id = request.data.get("region")
+        subregion_id = request.data.get("subregion")
+        if Church.objects.filter(
+            name=church_name, region_id=region_id, subregion_id=subregion_id
+        ).exists():
+            return Response(
+                {
+                    "error": "Church with this name already exists in the specified region."
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        return super().create(request, *args, **kwargs)
--- ffwpu_backend/region/views.py
+++ ffwpu_backend/region/views.py
@@ -1,5 +1,7 @@
 from rest_framework import viewsets
 from .serializers import *
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 
@@ -8,7 +10,30 @@ class RegionViewSet(viewsets.ModelViewSet):
     queryset = Region.objects.all()
     serializer_class = RegionSerializer
 
+    def create(self, request, *args, **kwargs):
+        # Check if the region name already exists
+        region_name = request.data.get("name")
+        if Region.objects.filter(name=region_name).exists():
+            return Response(
+                {"error": "Region with this name already exists."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        return super().create(request, *args, **kwargs)
+
 
 class SubregionViewSet(viewsets.ModelViewSet):
     queryset = Subregion.objects.all()
     serializer_class = SubregionSerializer
+
+    def create(self, request, *args, **kwargs):
+        # Check if the subregion name already exists
+        subregion_name = request.data.get("name")
+        region_id = request.data.get("region")
+        if Subregion.objects.filter(name=subregion_name, region_id=region_id).exists():
+            return Response(
+                {
+                    "error": "Subregion with this name already exists in the specified region."
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        return super().create(request, *args, **kwargs)
